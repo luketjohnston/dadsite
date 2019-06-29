5d76dc346e591bd46ca801da86ab0ed26ee04c24
Luke Johnston <luketjohnston32@gmail.com>
2019-06-29 01:15:31 -0700

    refactored gallery scrolling to prepare for responsive design

--- myapp/static/myapp/javascript/gallery.js
+++ myapp/static/myapp/javascript/gallery.js
@@ -1,46 +1,9 @@
 
-//var folder = "images/portraits";
-
-// Counts the number of portraits we have loaded
-//var imCount = 0;
-// number of portraits per page
-//const imPerPage = 6;
-//var buildingPage;
-
-var imPerPage = 6; // gonna have to compute from window size at some point
 var firstVisiblePortrait = 0;
 
 
-// counts the number of pages for the gallery 
-//var galPageCount = document.getElementById("gallery").childElementCount;
-
 const galDiv = document.getElementById("gallery");
 const numPortraits = galDiv.children.length;
-
-// in gallery.html, set portraits display: none, so set the first imPerPage visible
-for (i=0; i < imPerPage; i++) {
-  galDiv.children[i].style.display = 'initial';
-}
-
-
-
-
-//$.ajax({
-//    url : folder,
-//    success: function (data) {
-//        $(data).find("a").attr("href", function (i, val) {
-//            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-//                imCount += 1;
-//                if (imCount % imPerPage === 1) {
-//                  // make new page
-//                  buildingPage = galPage();
-//                  galDiv.appendChild(buildingPage);
-//                }
-//                buildingPage.appendChild(portrait(val));
-//            } 
-//        });
-//    }
-//});
 
 var currentPage = 0;
 
@@ -51,41 +14,45 @@ document.getElementById('next').addEventListener("click", nextClick);
 
 
 function nextClick() {
-  for (let i=0; i < imPerPage; i++) {
-    galDiv.children[firstVisiblePortrait].style.display = 'none';
-    firstVisiblePortrait++;
-    if (firstVisiblePortrait >= galDiv.children.length) {
-      firstVisiblePortrait = 0;
-      break; 
-    }
+  let i = 0;
+  // determine what portrait will be the first on the next page
+  while (i < numPortraits && (galDiv.children[i].offsetTop < 450 || 
+      galDiv.children[i].style.display === 'none')) { 
+    i++; 
   }
-  for (let i=0; i < imPerPage; i++) {
-    if (firstVisiblePortrait + i >= galDiv.children.length) { break; }
-    galDiv.children[firstVisiblePortrait + i].style.display = 'initial';
+
+  if (i === numPortraits) {
+    // go back to beginning, set all portraits visible again
+    for (i = 0; i < numPortraits; i++) {
+      galDiv.children[i].style.display = 'initial';
+    }
+    return;
+  }
+  for (let x=0; x < i; x++) {
+    galDiv.children[x].style.display = 'none';
   }
 }
 
 function previousClick() {
-  // TODO
-  for (let i=0; i < imPerPage; i++) {
-    galDiv.children[firstVisiblePortrait].style.display = 'none';
-    firstVisiblePortrait++;
-    if (firstVisiblePortrait >= galDiv.children.length) {
-      firstVisiblePortrait = 0;
-      break; 
-    }
+  // This is non-obvious to implement, but turns out pretty simple. We just
+  //  iterate backward through the children, setting their display to 'initial',
+  //  until the previous first visible portrait is no longer visible.
+  var i = 0; 
+  while (galDiv.children[i].style.display === 'none') {
+    i++;
   }
-  for (let i=0; i < imPerPage; i++) {
-    if (firstVisiblePortrait + i >= galDiv.children.length) { break; }
-    galDiv.children[firstVisiblePortrait + i].style.display = 'initial';
+  let firstVisiblePortraitI = i;
+  if (i === 0) {return;}
+  while (galDiv.children[firstVisiblePortraitI].offsetTop < 450) {
+    i--;
+    galDiv.children[i].style.display = 'initial';
   }
-
 }
+
 
 Number.prototype.mod = function(n) {
     return ((this%n)+n)%n;
 };
-
 
 
 
