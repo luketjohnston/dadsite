2a46800c5368b51ea532a0032b57fb2eaddcfd5f
Luke Johnston <lukejohnston@Lukes-MacBook-Pro.local>
2020-01-21 22:26:16 -0800

    portrait page arrows stay in same spot, portraits are centered

--- myapp/static/myapp/javascript/portrait.js
+++ myapp/static/myapp/javascript/portrait.js
@@ -3,15 +3,35 @@ resizeWindow();
 window.addEventListener("resize", resizeWindow);
 
 function resizeWindow() {
-  // first, make description same width as portrait
-  //width = document.getElementById('portrait').offsetWidth;
-  //textDiv = document.getElementById('textDiv');
-  //console.log(width)
 
-  //textDiv.style.width = width + 'px';
+  // calculate outerContainer height based on description height and 
+  // arrows height
+  var outerContainer = $("#outerContainer");
+  var descriptionHeight = $("#textDiv").outerHeight(true);
+  var arrowsHeight = $("#gallery_index").outerHeight(true);
+  var str = 'calc(100vh - ' + arrowsHeight.toString() + 'px' + ')';
+  outerContainer.height(str);
+  
+
+  // calculate image max-height based on description height
+  var containerHeight = $("#outerContainer").outerHeight(true);
+  var portrait = $("#portrait");
+  var maxH = containerHeight - descriptionHeight;
+  var maxH_str = maxH.toString() + 'px';
+
+  portrait.css('max-height', maxH_str);
+
+  //var portraitContainer = $("#portraitContainer");
+  //portraitContainer.css("top", "50%");
+  //portraitContainer.css("left", "50%");
+  //portraitContainer.css("transform", "translate(-50%, -50%)");
 
 
 }
+
+
+
+
 
 
   
