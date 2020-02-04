b7b4a4ae1ea3ee8bb4d299eb360c30d2d13570fe
Luke Johnston <lukejohnston@Lukes-MacBook-Pro.local>
2020-02-03 16:06:07 -0800

    about to make major changes to the way the portrait url works, commiting before

--- myapp/static/myapp/javascript/portrait.js
+++ myapp/static/myapp/javascript/portrait.js
@@ -1,33 +1,4 @@
-resizeWindow();
 
-window.addEventListener("resize", resizeWindow);
-
-function resizeWindow() {
-
-  // calculate outerContainer height based on description height and 
-  // arrows height
-  var outerContainer = $("#outerContainer");
-  var descriptionHeight = $("#textDiv").outerHeight(true);
-  var arrowsHeight = $("#gallery_index").outerHeight(true);
-  var str = 'calc(100vh - ' + arrowsHeight.toString() + 'px' + ')';
-  outerContainer.height(str);
-  
-
-  // calculate image max-height based on description height
-  var containerHeight = $("#outerContainer").outerHeight(true);
-  var portrait = $("#portrait");
-  var maxH = containerHeight - descriptionHeight;
-  var maxH_str = maxH.toString() + 'px';
-
-  portrait.css('max-height', maxH_str);
-
-  //var portraitContainer = $("#portraitContainer");
-  //portraitContainer.css("top", "50%");
-  //portraitContainer.css("left", "50%");
-  //portraitContainer.css("transform", "translate(-50%, -50%)");
-
-
-}
 
 
 
