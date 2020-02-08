87179a97d4d444cb98e946b403708d746f1e77c7
Luke Johnston <lukejohnston@Lukes-MacBook-Pro.local>
2020-02-08 14:38:32 -0800

    portrait page descriptions go multiline rather than overflow images

--- myapp/static/myapp/javascript/portrait.js
+++ myapp/static/myapp/javascript/portrait.js
@@ -7,11 +7,39 @@ var nextHash;
 var prevHash;
 
 
+window.onresize = updateDescriptionSizes;
+
+
+/* taken from stackexchange answer, https://stackoverflow.com/questions/52186493/how-can-we-get-new-image-size-dimension-after-giving-object-fitcontain-property */
+function getContainedSize(img) {
+  var ratio = img.naturalWidth/img.naturalHeight
+  var width = img.height*ratio
+  var height = img.height
+  if (width > img.width) {
+    width = img.width
+    height = img.width/ratio
+  }
+  return [width, height]
+}
+
+function updateDescriptionSizes() {
+  portraitsImgs = $(".slideimg");
+  console.log('here');
+  portraitsImgs.each(function(i) {
+    let myid = $(this).data('myid');
+    let width = getContainedSize(this)[0];
+    let description = $('.description.' + myid);
+    description.first().width(width);
+  });
+}
+    
+
 $(document).ready(function() {
 
   var maxDescriptionHeight = maxHeight($('.description'));
   $('#textDiv').height(maxDescriptionHeight);
   update();
+  updateDescriptionSizes();
 });
 
 // taken from stack exchange answer https://stackoverflow.com/questions/6060992/element-with-the-max-height-from-a-set-of-elements
@@ -19,6 +47,13 @@ function maxHeight(elems){
     return Math.max.apply(null, elems.map(function ()
     {
         return $(this).height();
+    }).get());
+}
+
+function maxWidth(elems){
+    return Math.max.apply(null, elems.map(function ()
+    {
+        return $(this).width();
     }).get());
 }
 
